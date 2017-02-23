7f5cd532a191c9f287bcdffc8650d0888a0a2dad
Mark Frost <frostmar@users.noreply.github.com>
2017-02-23 01:10:21 +0000

    angular service for data, and components for display

--- public/app.js
+++ public/app.js
@@ -1,34 +1,107 @@
 'use strict';
 
 
-// debug logger
-io().on('sensor_event', function (event) {
-  console.log('websocket sensor_event', event);
-});
-
-
 // Angular app level module which depends on views, and components
 var app = angular.module('rtl433webmon', [])
 
+// Angular service holding sensor data (from websocket)
+app.service('SensorDataService', function ($rootScope) {
+  console.log('sensorDataService constructing');
+  var self = this;
+  self.sensordata = {
+    CurrentCost: {},
+    Temperature: {}
+  };
 
+  self.socket = io.connect();
+  self.socket.on('sensor_event', function (event) {
+    // debug logging
+    console.log('websocket sensor_event', event);
+
+    if( event.model === 'CurrentCost TX' ){
+      // power sensor
+      self.sensordata.CurrentCost[event.dev_id] = event;
+    } else if ( event.model === 'Nexus Temperature/Humidity'  ||  event.model === 'WT450 sensor'){
+      // temperature/humidity sensor
+      self.sensordata.Temperature[event.id] = event;
+    }
+    $rootScope.$digest();
+  });
+
+  /**
+   * get data object for a particular temperature sensor
+   * @returns {object}
+   */
+  self.getTemperatureSensorData = function(deviceId){
+    return self.sensordata.Temperature[deviceId];
+  }
+
+  /**
+   * get data object for a particular temperature sensor
+   * @returns {object}
+   */
+  self.getPowerSensorData = function(deviceId){
+    return self.sensordata.CurrentCost[deviceId];
+  }
+
+});
+
+
+//////////////////////////////////
 // Angular components
 
-app.controller('SensorReading', function($scope) {
+app.component('temperatureDisplay', {
+  templateUrl: 'temperatureDisplay.html',
+  controller: TemperatureDisplayController,
+  bindings: {
+    deviceid: '<'
+  }
+});
 
-  var self = this;
-  self.event = {};
+function TemperatureDisplayController($scope, SensorDataService) {
+  var ctrl = this;
+  ctrl.temperature_C = undefined;
+  console.log('constructing TemperatureDisplayController for deviceid='+ctrl.deviceid);
 
-  function init(){
-    self.socket = io();
-    self.socket.on('sensor_event', function (event) {
-      if( event['dev_id'] &&
-          event['dev_id'] === 2115 )
-      {
-        self.event = event;
-        $scope.$apply();
-      }
-    });
-  };
-  init();
+  $scope.$watch(watchFunction, onDataChange);
 
-})
+  function watchFunction(){
+    return SensorDataService.getTemperatureSensorData(ctrl.deviceid);
+  }
+
+  function onDataChange(newValue, oldValue){
+    console.log('TemperatureDisplayController onDataChange for deviceid='+ctrl.deviceid);
+    if (newValue){
+      ctrl.temperature_C = newValue.temperature_C;
+    }
+  }
+}
+
+//////////////////////////////////
+
+app.component('powerDisplay', {
+  templateUrl: 'powerDisplay.html',
+  controller: PowerDisplayController,
+  bindings: {
+    deviceid: '<'
+  }
+});
+
+function PowerDisplayController($scope, SensorDataService) {
+  var ctrl = this;
+  ctrl.power = undefined;
+  console.log('constructing PowerDisplayController for deviceid='+ctrl.deviceid);
+
+  $scope.$watch(watchFunction, onDataChange);
+
+  function watchFunction(){
+    return SensorDataService.getPowerSensorData(ctrl.deviceid);
+  }
+
+  function onDataChange(newValue, oldValue){
+    console.log('PowerDisplayController onDataChange for deviceid='+ctrl.deviceid);
+    if (newValue){
+      ctrl.power = newValue.power0;
+    }
+  }
+}
