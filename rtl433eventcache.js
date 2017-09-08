146e37401fd50037b1fb252dd2f87a99d68e8062
Mark Frost <frostmar@users.noreply.github.com>
2017-09-08 21:50:37 +0100

    ES6 class syntax

--- rtl433eventcache.js
+++ rtl433eventcache.js
@@ -1,38 +1,41 @@
 'use strict'
 
-/**
- * Cache of last event of each sensor
- * (identified by key made from model and an id field)
- * @constructor
- */
-function Rtl433EventCache () {
-  this.cache = {} // cache: <id> => <event>
-};
+class Rtl433EventCache {
+  /**
+   * Cache of last event of each sensor
+   * (identified by key made from model and an id field)
+   * @constructor
+   */
+  constructor () {
+    this.cache = {} // cache: <id> => <event>
+  };
 
-/**
- * store an event
- * @param {object} newEvent - rtl433adapter event message
- */
-Rtl433EventCache.prototype.store = function (newEvent) {
-  const key = this.getEventKey(newEvent)
-  this.cache[key] = newEvent
-}
+  /**
+   * store an event
+   * @param {object} newEvent - rtl433adapter event message
+   */
+  store (newEvent) {
+    const key = this.getEventKey(newEvent)
+    this.cache[key] = newEvent
+  }
 
-/**
- * @private
- * @param {object} event - rtl433adapter event message
- * @returns {string} key for the event
- */
-Rtl433EventCache.prototype.getEventKey = function (event) {
-  const id = event.id ? event.id : event.dev_id
-  return event.model + '|' + id
-}
+  /**
+   * @private
+   * @param {object} event - rtl433adapter event message
+   * @returns {string} key for the event
+   */
+  getEventKey (event) {
+    const id = event.id ? event.id : event.dev_id
+    return event.model + '|' + id
+  }
 
-/**
- * @return {object[]} array of stored events
- */
-Rtl433EventCache.prototype.getEvents = function () {
-  return Object.values(this.cache)
+  /**
+   * @return {object[]} array of stored events
+   */
+  getEvents () {
+    return Object.values(this.cache)
+  }
+
 }
 
 module.exports = Rtl433EventCache
