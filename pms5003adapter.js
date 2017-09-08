8bf78a889da3abeb83506b38f49407bfc045f192
Mark Frost <frostmar@users.noreply.github.com>
2017-09-08 21:06:16 +0100

    ES6 class syntax

--- pms5003adapter.js
+++ pms5003adapter.js
@@ -3,69 +3,71 @@
 const debug = require('debug')('webmon:pms5003adapter')
 const EventEmitter = require('events').EventEmitter
 const SerialPort = require('serialport')
-const util = require('util')
 
 const frameHeader = Buffer.from([0x42, 0x4d])
 
-util.inherits(Pms5003adapter, EventEmitter)
+class Pms5003adapter extends EventEmitter {
 
-/**
- * Adapter for a Plantower PMS5003 air quality sensor (parsing data from a serial port).
- * This class is an EventEmitter: parses the sensor data and emits
- * 'sensor_event' messages
- * @constructor
- * @param {object} options
- * @param {string} options.serialdevice - device to read eg '/dev/serial0'
- */
-function Pms5003adapter (options) {
-  debug('constructed with options: %o', options)
-  let serialdata = Buffer.from([])
+  /**
+   * Adapter for a Plantower PMS5003 air quality sensor (parsing data from a serial port).
+   * This class is an EventEmitter: parses the sensor data and emits
+   * 'sensor_event' messages
+   * @constructor
+   * @param {object} options
+   * @param {string} options.serialdevice - device to read eg '/dev/serial0'
+   */
+  constructor (options) {
+    super()
+    debug('constructed with options: %o', options)
+    let serialdata = Buffer.from([])
 
-  const port = new SerialPort(options.serialdevice, {baudrate: 9600})
-  port.on('data', (newdata) => {
-    debug(`serial data received: ${newdata.length} bytes`)
-    serialdata = Buffer.concat([serialdata, newdata])
-    const start = serialdata.indexOf(frameHeader)
-    if (start > 0) {
-      serialdata = serialdata.slice(start)
-    }
-    if (serialdata.length >= 32) {
-      const frameBuffer = serialdata.slice(0, 32)
-      serialdata = serialdata.slice(32)
-      let frame = this.readFrame(frameBuffer)
-      frame.time = new Date().toISOString()
-      frame.model = 'pms5003'
-      this.emit('sensor_event', frame)
-    }
-  })
-}
-
-/**
- * Read a Plantower PMS5003 air-quality sensor data packet
- * http://www.aqmd.gov/docs/default-source/aq-spec/resources-page/plantower-pms5003-manual_v2-3.pdf
- * @param {Buffer} buffer - data from serial port to parse
- * @returns {object} air quality data structure
- */
-Pms5003adapter.prototype.readFrame = function (buffer) {
-  debug('getPacket() entry')
-  const frame = {
-    'framelength': buffer.readInt16BE(2),
-    'apm1': buffer.readInt16BE(4),  // Data  1 refers to PM1.0 concentration unit μg/m3 (CF=1，standard particle)
-    'apm2_5': buffer.readInt16BE(6),  // Data  2 refers to PM2.5 concentration unit μg/m3 (CF=1，standard particle)
-    'apm10': buffer.readInt16BE(8),  // Data  3 refers to PM10  concentration unit μg/m3 (CF=1，standard particle)
-    'pm1': buffer.readInt16BE(10), // Data  4 refers to PM1.0 concentration unit μg/m3 (under atmospheric environment)
-    'pm2_5': buffer.readInt16BE(12), // Data  5 refers to PM2.5 concentration unit μg/m3 (under atmospheric environment)
-    'pm10': buffer.readInt16BE(14), // Data  6 refers to PM10  concentration unit μg/m3 (under atmospheric environment)
-    'gt0_3um': buffer.readInt16BE(16), // Data  7 indicates the number of particles with diameter beyond  0.3 um in 0.1 L of air
-    'gt0_5um': buffer.readInt16BE(18), // Data  8 indicates the number of particles with diameter beyond  0.5 um in 0.1 L of air
-    'gt1um': buffer.readInt16BE(20), // Data  9 indicates the number of particles with diameter beyond  1.0 um in 0.1 L of air
-    'gt2_5um': buffer.readInt16BE(22), // Data 10 indicates the number of particles with diameter beyond 2.5 um in 0.1 L of air
-    'gt5um': buffer.readInt16BE(24), // Data 11 indicates the number of particles with diameter beyond 5.0 um in 0.1 L of air
-    'gt10um': buffer.readInt16BE(26), // Data 12 indicates the number of particles with diameter beyond 10  um in 0.1 L of air
-    'data13_reserved': buffer.readInt16BE(28), // Data 13 reserved
-    'check': buffer.readInt16BE(30) // Data 14 Check code = Start character 1 + Start character 2 + Data 1... + Data 13
+    const port = new SerialPort(options.serialdevice, {baudrate: 9600})
+    port.on('data', (newdata) => {
+      debug(`serial data received: ${newdata.length} bytes`)
+      serialdata = Buffer.concat([serialdata, newdata])
+      const start = serialdata.indexOf(frameHeader)
+      if (start > 0) {
+        serialdata = serialdata.slice(start)
+      }
+      if (serialdata.length >= 32) {
+        const frameBuffer = serialdata.slice(0, 32)
+        serialdata = serialdata.slice(32)
+        let frame = this.readFrame(frameBuffer)
+        frame.time = new Date().toISOString()
+        frame.model = 'pms5003'
+        this.emit('sensor_event', frame)
+      }
+    })
   }
-  return frame
+
+  /**
+   * Read a Plantower PMS5003 air-quality sensor data packet
+   * http://www.aqmd.gov/docs/default-source/aq-spec/resources-page/plantower-pms5003-manual_v2-3.pdf
+   * @param {Buffer} buffer - data from serial port to parse
+   * @returns {object} air quality data structure
+   */
+  readFrame (buffer) {
+    debug('getPacket() entry')
+    const frame = {
+      'framelength': buffer.readInt16BE(2),
+      'apm1': buffer.readInt16BE(4),  // Data  1 refers to PM1.0 concentration unit μg/m3 (CF=1，standard particle)
+      'apm2_5': buffer.readInt16BE(6),  // Data  2 refers to PM2.5 concentration unit μg/m3 (CF=1，standard particle)
+      'apm10': buffer.readInt16BE(8),  // Data  3 refers to PM10  concentration unit μg/m3 (CF=1，standard particle)
+      'pm1': buffer.readInt16BE(10), // Data  4 refers to PM1.0 concentration unit μg/m3 (under atmospheric environment)
+      'pm2_5': buffer.readInt16BE(12), // Data  5 refers to PM2.5 concentration unit μg/m3 (under atmospheric environment)
+      'pm10': buffer.readInt16BE(14), // Data  6 refers to PM10  concentration unit μg/m3 (under atmospheric environment)
+      'gt0_3um': buffer.readInt16BE(16), // Data  7 indicates the number of particles with diameter beyond  0.3 um in 0.1 L of air
+      'gt0_5um': buffer.readInt16BE(18), // Data  8 indicates the number of particles with diameter beyond  0.5 um in 0.1 L of air
+      'gt1um': buffer.readInt16BE(20), // Data  9 indicates the number of particles with diameter beyond  1.0 um in 0.1 L of air
+      'gt2_5um': buffer.readInt16BE(22), // Data 10 indicates the number of particles with diameter beyond 2.5 um in 0.1 L of air
+      'gt5um': buffer.readInt16BE(24), // Data 11 indicates the number of particles with diameter beyond 5.0 um in 0.1 L of air
+      'gt10um': buffer.readInt16BE(26), // Data 12 indicates the number of particles with diameter beyond 10  um in 0.1 L of air
+      'data13_reserved': buffer.readInt16BE(28), // Data 13 reserved
+      'check': buffer.readInt16BE(30) // Data 14 Check code = Start character 1 + Start character 2 + Data 1... + Data 13
+    }
+    return frame
+  }
+
 }
 
 module.exports = Pms5003adapter
--- rtl433adapter.js
+++ rtl433adapter.js
@@ -2,55 +2,53 @@
 
 const debug = require('debug')('webmon:rtl433adapter')
 const EventEmitter = require('events').EventEmitter
-const util = require('util')
 const spawn = require('child_process').spawn
 
-util.inherits(Rtl433adapter, EventEmitter)
+class Rtl433adapter extends EventEmitter {
 
-/**
- * Adapter for a rtl433 child process (decoding 433MHz radio device messages).
- * This class is an EventEmitter: parses the child process's stdout and emits
- * 'sensor_event' messages
- * @constructor
- * @param {object}   options
- * @param {Number[]} options.devices - rtl_433 device numbers (-R parameters) to listen for
- */
-function Rtl433adapter (options) {
-  const self = this
-  let lineBuffer = ''
-  this.options = options
-  debug('constructed with options: %o', options)
+  /**
+   * Adapter for a rtl433 child process (decoding 433MHz radio device messages).
+   * This class is an EventEmitter: parses the child process's stdout and emits
+   * 'sensor_event' messages
+   * @constructor
+   * @param {object}   options
+   * @param {Number[]} options.devices - rtl_433 device numbers (-R parameters) to listen for
+   */
+  constructor (options) {
+    super()
+    let lineBuffer = ''
+    this.options = options
+    debug('constructed with options: %o', options)
 
-  // call the super constructor to initialize `this`
-  EventEmitter.call(this)
+    // spawn child process
+    this.childProcess = this.spawnChild()
 
-  // spawn child process
-  this.childProcess = this.spawnChild()
+    // subclass extensions
 
-  // subclass extensions
+    this.childProcess.stdout.on('data', function (data) {
+      lineBuffer += data
+      let newlinePos = lineBuffer.indexOf('\n')
+      if (newlinePos !== -1) {
+        let readingString = lineBuffer.substring(0, newlinePos)
+        lineBuffer = lineBuffer.substring(newlinePos + 1)
+        const reading = JSON.parse(readingString)
+        this.emit('sensor_event', reading)
+      }
+    })
+  }
 
-  this.childProcess.stdout.on('data', function (data) {
-    lineBuffer += data
-    let newlinePos = lineBuffer.indexOf('\n')
-    if (newlinePos !== -1) {
-      let readingString = lineBuffer.substring(0, newlinePos)
-      lineBuffer = lineBuffer.substring(newlinePos + 1)
-      const reading = JSON.parse(readingString)
-      self.emit('sensor_event', reading)
-    }
-  })
-};
+  /**
+   * spawn a child process
+   * @private
+   * @returns {ChildProcess}
+   */
+  spawnChild () {
+    const args = ['-F', 'json']
+    this.options.devices.forEach((device) => { args.push('-R'); args.push(device) })
+    debug('spawning rtl_433 with args: ', args)
+    return spawn('rtl_433', args)
+  }
 
-/**
- * spawn a child process
- * @private
- * @returns {ChildProcess}
- */
-Rtl433adapter.prototype.spawnChild = function () {
-  const args = ['-F', 'json']
-  this.options.devices.forEach((device) => { args.push('-R'); args.push(device) })
-  debug('spawning rtl_433 with args: ', args)
-  return spawn('rtl_433', args)
 }
 
 module.exports = Rtl433adapter
